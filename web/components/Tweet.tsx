b07ee8c3adfe7b88ba2f655a6d6da6a68802a5cc
T-pl <tiagofqu@gmail.com>
2022-11-02 22:01:56 -0300

    First Commit of the project

---
+++ web/components/Tweet.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Tweet() {
+  return (
+    <div>
+      TESTANDO
+      <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro voluptatum fugiat a totam pariatur quas obcaecati eaque ratione nam, fuga laborum dignissimos nemo iste? In cumque blanditiis ex pariatur ut?</p>
+    </div>
+  )
+}
+
+export default Tweet
